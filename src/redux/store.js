2943ca1697a862b536a2fd6f6c54795f1ff235e0
Yuriy Yaremkiv <y.yaremkiv@gmail.com>
2022-11-28 15:56:38 +0200

    update

---
+++ src/redux/store.js
@@ -0,0 +1,16 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { createReducer, createAction } from '@reduxjs/toolkit';
+
+export const increment = createAction('myValue/increment');
+export const decrement = createAction('myValue/decrement');
+
+const myReducer = createReducer(0, {
+  [increment]: (state, action) => state + action.payload,
+  [decrement]: (state, action) => state - action.payload,
+});
+
+export const store = configureStore({
+  reducer: {
+    myValue: myReducer,
+  },
+});
