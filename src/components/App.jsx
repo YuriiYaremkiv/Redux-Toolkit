a8043b38ef774a6f2a9367e5627f1ac6aa061453
Yuriy Yaremkiv <y.yaremkiv@gmail.com>
2022-11-29 14:53:51 +0200

    redux

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,3 @@
-import { useSelector, useDispatch } from 'react-redux';
-import { update, getvalue } from 'redux/clicksSlice';
-
 export const App = () => {
-  const dispatch = useDispatch();
-  const numberOfCkicks = useSelector(getvalue);
-  return (
-    <div>
-      <h1>Number of clicks: {numberOfCkicks}</h1>
-      <button type="submit" onClick={() => dispatch(update(5))}>
-        Add 5 clicks
-      </button>
-      <button type="submit" onClick={() => dispatch(update(10))}>
-        Add 10 clicks
-      </button>
-      <button type="submit" onClick={() => dispatch(update(20))}>
-        Add 20 clicks
-      </button>
-    </div>
-  );
+  return <div>App</div>;
 };
